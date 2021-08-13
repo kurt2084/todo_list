f56d453366343b9fb2df33ca815897e61d95748b
kurt2084 <kurt2084@gmail.com>
2021-08-13 14:45:53 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+
 const app = express()
-const mongoose = require('mongoose') // 載入 mongoose
 
 // 資料庫連線
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -18,9 +20,14 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// 樣板資料
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // 設定首頁路由
+// Todo 首頁
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定 port 3000
