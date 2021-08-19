bc0c1d673c24773bfea1fd07b844c3188407ee4f
KurtTang <kurt2084@gmail.com>
2021-08-19 13:04:41 +0800

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
 const todoSchema = new Schema({
   name: {
     type: String, // 資料型別是字串
@@ -7,7 +8,7 @@ const todoSchema = new Schema({
   },
   isDone: {
     type: Boolean,
-    default: false  // 預設完成狀態為 false
+    default: false
   }
 })
 module.exports = mongoose.model('Todo', todoSchema)
